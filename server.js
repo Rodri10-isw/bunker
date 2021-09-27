6f2b3ed8f21ba72f82e7393ac71961f3a1993734
Rodri10-isw <38498075+Rodri10-isw@users.noreply.github.com>
2021-09-27 17:33:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const { Server } = require("net");
 const server = new Server();
-
+server.on("connection", (socket) => {
+     console.log('Nueva coneccion de: ', socket.remoteAddress + " " + socket.remotePort);
+     socket.setEncoding("utf-8");
+     socket.on("data", (data) => {
+          console.log(data);
+          var cadena = data;
+          var arrayDeCadenas = cadena.split(";");
+          for (var i = 0; i < arrayDeCadenas.length; i++) {
+                console.log("Cadena " + [i] + ": " + arrayDeCadenas[i]);
+           }
+     });
+     socket.on("close", () => {
+          console.log("Dispositivo fuera del server" + socket.remoteAddress)
+     });
+});
 
 server.listen({ port: 8080 }, () => {
      console.log("escuchando en el puerto  gps 8000");
