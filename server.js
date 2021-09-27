9eccf72fdcce9b26b843bcdff8f0ceff7898ae4a
Rodri10-isw <38498075+Rodri10-isw@users.noreply.github.com>
2021-09-27 17:26:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const server = new Server();
 
 
 server.listen({ port: 8000 }, () => {
-     console.log("escuchando en el puerto 8000");
+     console.log("escuchando en el puerto  gps 8000");
 });
