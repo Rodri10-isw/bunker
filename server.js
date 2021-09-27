0d243bdc861cbdf08b9af8d0dbe8f671dd991faa
Rodri10-isw <38498075+Rodri10-isw@users.noreply.github.com>
2021-09-27 17:18:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const { Server } = require("net");
 const server = new Server();
 
 
-server.listen({ port: 8080 }, () => {
+server.listen({ port: 8000 }, () => {
      console.log("escuchando en el puerto 8000");
 });
